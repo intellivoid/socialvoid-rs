44c90f4d9a32d89ae525dbcadb29530765a6b56a
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-09-17 20:51:06 +0530

    added rest of the validation errors

--- rawclient/src/error/errors.rs
+++ rawclient/src/error/errors.rs
@@ -10,11 +10,27 @@ enum_from_primitive! {
 }
 
 enum_from_primitive! {
-#[derive(Debug)]
-pub enum ValidationError {
-    InvalidUsername = 8448,
-    InvalidPassword = 8449,
-}
+    #[derive(Debug)]
+    pub enum ValidationError {
+        InvalidUsername = 8448,
+        InvalidPassword = 8449,
+        InvalidFirstName = 8450,
+        InvalidLastName = 8451,
+        InvalidBiography = 8452,
+        UsernameAlreadyExists = 8453,
+        InvalidPeerInput = 8454,
+        InvalidPostText = 8455,
+        InvalidClientPublicHash = 8456,
+        InvalidClientPrivateHash = 8457,
+        InvalidPlatform = 8458,
+        InvalidVersion = 8459,
+        InvalidClientName = 8460,
+        InvalidSessionIdentification = 8461,
+        InvalidFileForProfilePicture = 8462,
+        FileTooLarge = 8463,
+        InvalidHelpDocumentId = 8464,
+        AgreementRequired = 8465,
+    }
 }
 
 enum_from_primitive! {
