6e343c1d799c91ef2acc1e49257efa60acc2200b
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-10-10 14:41:47 +0530

    Refactored + delete a session if expired or not found

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,8 @@
 use client as sv_client;
-use serde::{Deserialize, Serialize};
-use std::io::{stdin, stdout, Write};
 use structopt::StructOpt;
+
+mod utils;
+use crate::utils::*;
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +16,7 @@ async fn main() {
 
     match args {
         Cli::Login { username } => {
-            load_session_and_new_session_if_notfound_or_panic(&config, &mut sv).await;
+            setup_sessions(&config, &mut sv, &mut current_session).await;
             let username = if let Some(username) = username {
                 username
             } else {
@@ -29,8 +30,7 @@ async fn main() {
                 current_session
             };
             current_session = sk;
-            // maybe have a way to check if username already exists ??
-            let password = prompt_stdin("Enter password: ");
+            let password = prompt_password("Enter password: ");
             //TODO: add OTP support
             match sv.authenticate_user(sk, username, password, None).await {
                 Err(err) => {
@@ -43,7 +43,7 @@ async fn main() {
         }
         Cli::Config { .. } => {}
         Cli::GetMe => {
-            load_session_and_new_session_if_notfound_or_panic(&config, &mut sv).await;
+            setup_sessions(&config, &mut sv, &mut current_session).await;
 
             match sv.get_me(current_session).await {
                 Ok(response) => println!("{:?}", response),
@@ -58,84 +58,10 @@ async fn main() {
     save_config(current_session, &config).expect("Couldn't save the config");
 }
 
-async fn load_session_and_new_session_if_notfound_or_panic(
-    config: &Config,
-    sv: &mut sv_client::Client,
-) {
-    if let Err(err) = sv.load_sessions(&config.sessions_file) {
-        if err.kind() == std::io::ErrorKind::NotFound {
-            sv.new_session()
-                .await
-                .expect("Couldn't create a new session.");
-        } else {
-            panic!("Couldn't load sessions from the config\n{:?}", err);
-        }
-    }
-}
-
 #[derive(Debug, StructOpt)]
 enum Cli {
     Login { username: Option<String> },
     Config { server: Option<usize> },
     GetMe,
     Sync {},
-}
-
-fn load_config() -> Config {
-    //TODO: Use config from a file
-    let base_path = match std::env::var("SV_CLI_PATH") {
-        Ok(val) => shellexpand::full(&val).unwrap().to_string(),
-        Err(_) => {
-            let se = shellexpand::tilde("~/.sv-cli").to_string();
-            std::fs::create_dir_all(&se).expect("Failed to create directory at ~/.sv-cli");
-            se
-        }
-    };
-
-    let config_path = format!("{}/config.json", base_path);
-    match std::fs::read_to_string(&config_path) {
-        Ok(contents) => match serde_json::from_str(&contents) {
-            Ok(data) => data,
-            Err(err) => {
-                panic!("Error processing JSON in config file.\n{:?}", err)
-            }
-        },
-        Err(_err) => Config {
-            sessions_file: format!("{}/sessions", base_path),
-            base_path: base_path,
-            config_path,
-            current_session: 0,
-        },
-    }
-}
-
-fn save_config(sesh_key: usize, config: &Config) -> Result<(), std::io::Error> {
-    let mut config = config.clone();
-    config.current_session = sesh_key;
-    std::fs::write(&config.config_path, serde_json::to_string(&config).unwrap())?;
-    Ok(())
-}
-
-fn prompt_stdin(prompt: &str) -> String {
-    print!("{}", prompt);
-    let mut s = String::new();
-    let _ = stdout().flush();
-    stdin()
-        .read_line(&mut s)
-        .expect("Did not enter a valid username"); //TODO: validation?
-    if let Some('\n') = s.chars().next_back() {
-        s.pop();
-    }
-    if let Some('\r') = s.chars().next_back() {
-        s.pop();
-    }
-    s
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Config {
-    sessions_file: String,
-    base_path: String,
-    config_path: String,
-    current_session: usize,
 }
---
+++ cli/src/utils.rs
@@ -0,0 +1,103 @@
+use client as sv_client;
+use rawclient::{AuthenticationError, ErrorKind};
+use rpassword::read_password;
+use serde::{Deserialize, Serialize};
+use std::io::{stdin, stdout, Write};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    pub sessions_file: String,
+    pub base_path: String,
+    pub config_path: String,
+    pub current_session: usize,
+}
+
+pub async fn setup_sessions(config: &Config, sv: &mut sv_client::Client, sesh_key: &mut usize) {
+    if let Err(err) = sv.load_sessions(&config.sessions_file) {
+        if err.kind() == std::io::ErrorKind::NotFound {
+            sv.new_session()
+                .await
+                .expect("Couldn't create a new session.");
+        } else {
+            panic!("Couldn't load sessions from the config\n{:?}", err);
+        }
+    }
+
+    match sv.get_session(*sesh_key).await {
+        Ok(_) => {}
+        Err(err) => match err.kind {
+            ErrorKind::Authentication(AuthenticationError::SessionExpired)
+            | ErrorKind::Authentication(AuthenticationError::SessionNotFound) => {
+                println!("Session either didn't exist or is expired.\nDeleting it and creating a new one.");
+                sv.delete_session(*sesh_key).await;
+                let new_sesh_key = sv
+                    .new_session()
+                    .await
+                    .expect("Couldn't create a new session.");
+                *sesh_key = new_sesh_key;
+            }
+            _ => {
+                panic!(
+                    "Couldn't `get` session. The session is probably corrupt.
+Either delete the sessions file or fix the corrupt session."
+                );
+            }
+        },
+    }
+}
+
+pub fn load_config() -> Config {
+    //TODO: Use config from a file
+    let base_path = match std::env::var("SV_CLI_PATH") {
+        Ok(val) => shellexpand::full(&val).unwrap().to_string(),
+        Err(_) => {
+            let se = shellexpand::tilde("~/.sv-cli").to_string();
+            std::fs::create_dir_all(&se).expect("Failed to create directory at ~/.sv-cli");
+            se
+        }
+    };
+
+    let config_path = format!("{}/config.json", base_path);
+    match std::fs::read_to_string(&config_path) {
+        Ok(contents) => match serde_json::from_str(&contents) {
+            Ok(data) => data,
+            Err(err) => {
+                panic!("Error processing JSON in config file.\n{:?}", err)
+            }
+        },
+        Err(_err) => Config {
+            sessions_file: format!("{}/sessions", base_path),
+            base_path: base_path,
+            config_path,
+            current_session: 0,
+        },
+    }
+}
+
+pub fn save_config(sesh_key: usize, config: &Config) -> Result<(), std::io::Error> {
+    let mut config = config.clone();
+    config.current_session = sesh_key;
+    std::fs::write(&config.config_path, serde_json::to_string(&config).unwrap())?;
+    Ok(())
+}
+
+pub fn prompt_stdin(prompt: &str) -> String {
+    print!("{}", prompt);
+    let mut s = String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Couldn't read string"); //TODO: validation?
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    s
+}
+
+pub fn prompt_password(prompt: &str) -> String {
+    print!("{}", prompt);
+    std::io::stdout().flush().unwrap();
+    let password = read_password().expect("Couldn't read the password");
+    password
+}
